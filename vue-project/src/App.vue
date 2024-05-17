4e782d2751c0617c73287c50076a2370cb37ede5
belen001 <b.bravo02@ufromail.cl>
2024-05-17 15:55:53 -0400

    Tutorial 2

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,50 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import Component1 from './components/Component1.vue';
+import ComponentePadre from './components/ComponentePadre.vue';
+
 </script>
 
 <template>
-  <router-view/>
-  <Component1 />
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <ComponentePadre />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- vue-project/src/router/router.js
+++ vue-project/src/router/router.js
@@ -1,10 +1,28 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/components/V-if.vue";
 import Component1 from "@/components/Component1.vue";
+import ComponentePadre from '@/components/ComponentePadre.vue';
+import ComponenteHijo from '@/components/ComponenteHijo.vue';
+import Bienvenida from '@/components/Bienvenida.vue';
+import VBind from '@/components/V-bind.vue';
+import VFor from '@/components/V-for.vue';
+import VIf from '@/components/V-if.vue';
+import VModel from '@/components/V-model.vue';
+import VOn from '@/components/V-on.vue';
+import VShow from '@/components/V-show.vue';
 
 const routes = [
 { path: '/', component: HelloWorld },
-{ path: '/', component: Component1 }
+{ path: '/component1', component: Component1 },
+{ path: '/componentePadre', component: ComponentePadre },
+{ path: '/ComponenteHijo', component: ComponenteHijo },
+{ path: '/Bienvenida', component: Bienvenida },
+{ path: '/VBind', component: VBind },
+{ path: '/VFor', component: VFor },
+{ path: '/VIf', component: VIf },
+{ path: '/VModel', component: VModel },
+{ path: '/VOn', component: VOn },
+{ path: '/VShow', component: VShow }
 ]
 
 export const router = createRouter({
